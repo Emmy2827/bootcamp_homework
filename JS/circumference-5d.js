63fa4351052830774fd340d1d9a977e491ee64a7
Emily <debbiegirldan@gmail.com>
2023-04-28 15:06:03 -0400

    circumference

---
+++ JS/circumference-5d.js
@@ -0,0 +1,14 @@
+function calcCircumference(radius)
+{
+    var calcCircumference = 3.14 * 2 * radius;
+    console.log(" The Circumference is " + calcCircumference);
+}
+calcCircumference(5)
+function calcArea(radius)
+{
+    var calcArea = 3.14 * radius * radius;
+    console.log(" The area is " + calcArea);
+}
+
+calcArea(31.41)
+
